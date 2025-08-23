52da9d233836be56cc08d60d44fce1de5e8c273a
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-08-23 23:04:29 +0200

    Finalize Home Feature, Connexion and Register feature.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,6 +18,14 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        if ($this->getUser()) {
+            $this->addFlash(
+                'warning',
+                'Vous êtes déjà connecté, veuillez-vous déconnecter.'
+            );
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
@@ -27,6 +35,7 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
+        $this->addFlash('danger', 'Vous vous êtes déconnecté.');
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
