e6833c51ddf006473510cd79e3e947031f9e0c53
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-09-11 18:55:15 +0200

    Adding kickingUser features on app

--- src/Controller/CarpoolController.php
+++ src/Controller/CarpoolController.php
@@ -9,7 +9,9 @@ use App\Form\CarpoolType;
 use App\Repository\CarpoolingRepository;
 use App\Repository\CarRepository;
 use App\Repository\ParticipationRepository;
+use App\Repository\UserRepository;
 use App\Services\GeolocationService;
+use App\Services\SendEmailService;
 use DateInterval;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -165,7 +167,7 @@ class CarpoolController extends AbstractController
     }
 
     #[Route('/mycarpool/details/{id}', name: 'app_carpool_details', requirements: ['id' => '\d+'])]
-    public function detailsMyCarpool(int $id, CarpoolingRepository $carpoolRep): Response
+    public function detailsMyCarpool(int $id, CarpoolingRepository $carpoolRep, ParticipationRepository $participationRep): Response
     {
         /** @var User $user */
         $user = $this->getUser();
@@ -179,19 +181,44 @@ class CarpoolController extends AbstractController
         }
 
         $carpool = $carpoolRep->find($id);
-
+        $allParticipation = $participationRep->findBy(['carpooling' => $carpool]);
         return $this->render('carpool/detail.html.twig', [
             'user' => $user,
             'carpool' => $carpool,
+            'allParticipation' => $allParticipation
         ]);
     }
 
-    #[Route('/carpooljoined/detail/{id}', name: 'app_carpool_joined_details', requirements: ['id' => '\d+'])]
-    public function detailsJoinedCarpool(int $id, CarpoolingRepository $carpoolRep, ParticipationRepository $participationRep): Response
-    {
-        /** @var User $user */
-        $user = $this->getUser();
+    // #[Route('/carpooljoined/detail/{id}', name: 'app_carpool_joined_details', requirements: ['id' => '\d+'])]
+    // public function detailsJoinedCarpool(int $id, CarpoolingRepository $carpoolRep, ParticipationRepository $participationRep): Response
+    // {
+    //     /** @var User $user */
+    //     $user = $this->getUser();
 
+    //     if ($user === null) {
+    //         $this->addFlash(
+    //             'danger',
+    //             'Vous ne pouvez pas accéder à cette page tant que vous n\'êtes pas connecté(e).'
+    //         );
+    //         return $this->redirectToRoute('app_login');
+    //     }
+
+    //     $carpool = $carpoolRep->find($id);
+
+    //     return $this->render('fileName.html.twig', []);
+    // }
+
+    #[Route('mycarpool/{carpool_id}/kickuser/{user_id}',name:'app_mycarpool_kick_user',requirements:['carpool_id' => '\d+', 'user_id' =>'\d+'])]
+    public function kickUserFromCarpool(
+        int $carpool_id, 
+        int $user_id,
+        CarpoolingRepository $carpoolRep,
+        ParticipationRepository $partipRep, 
+        UserRepository $userRep,
+        EntityManagerInterface $em,
+        SendEmailService $sendEmail,){
+
+        $user = $this->getUser();
         if ($user === null) {
             $this->addFlash(
                 'danger',
@@ -199,9 +226,54 @@ class CarpoolController extends AbstractController
             );
             return $this->redirectToRoute('app_login');
         }
+        /** @var Carpooling $carpool */
+        $carpool = $carpoolRep->find($carpool_id);
 
-        $carpool = $carpoolRep->find($id);
+        /** @var User $kickedUser */
+        $kickedUser = $userRep->find($user_id);
 
-        return $this->render('fileName.html.twig', []);
+        /** @var Participation $participation */
+        $participation = $partipRep->findOneBy(['user' => $kickedUser, 'carpooling' => $carpool]);
+
+        if($carpool->getCreatedBy() !== $user || $carpool->getStatut() !== 'Online' || $carpool->getStartDate() < new DateTimeImmutable('-2 hours') || $participation === null)
+        {
+            $this->addFlash(
+                'danger',
+                'Un problème est survenue avec le lien, si le problème persiste contact l\'administrateur.'
+            );
+            return $this->redirectToRoute('app_carpool_index');
+        }
+        
+        //Après toutes les vérifications (sécurité), on supprime la participation du User
+        $em->remove($participation);
+        $em->flush();
+
+        //On rembourse le User
+        $kickedUser->setEcopiece($kickedUser->getEcopiece() + $carpool->getPricePerPerson());
+        $em->persist($kickedUser);
+        $em->flush();
+
+        //On remet à jour le nombre de siège sur le Carpool
+        $carpool->setAvailableSeat($carpool->getAvailableSeat() + 1);
+        $em->persist($carpool);
+        $em->flush();
+
+        //On envoie un mail au User expulsé de la participation pour le prévenir.
+        $sendEmail->send(
+                'no-reply@ecoride-project.test',
+                $kickedUser->getEmail(),
+                "IMPORTANT - l'hôte d'un de vos trajets prévus vous a expulsé de sa course",
+                'kick_carpool',
+                compact('carpool', 'user','kickedUser')
+            );
+        
+        //On recharge la page de détail du User
+        $this->addFlash(
+            'success',
+            "La participation de l'utilisateur " . $kickedUser->getUsername() . " a bien été supprimé, un mail lui a été addressé pour le prévenir."
+        );
+        return $this->redirectToRoute('app_carpool_index');
     }
+
+
 }
