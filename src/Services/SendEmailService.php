3620108053b13dc06f3379d549aac01301e6f815
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-09-21 20:58:50 +0200

    add sendEmailService features

--- src/Services/SendEmailService.php
+++ src/Services/SendEmailService.php
@@ -2,10 +2,19 @@
 
 namespace App\Services;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
 class SendEmailService
 {
-    public function send(string $to, string $subject, string $content, ?string $from = null): void 
+    public function __construct(private MailerInterface $mailer) {}
+    public function send(string $from, string $to, string $subject, string $template, array $content): void
     {
-        
+        $email = new TemplatedEmail()->from($from)
+            ->to($to)
+            ->subject($subject)
+            ->htmlTemplate("emails/$template.html.twig")
+            ->context($content);
+        $this->mailer->send($email);
     }
 }
