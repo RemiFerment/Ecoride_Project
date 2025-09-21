6c254ad2e1e00f89a84fc14e4f30696a280b7bc1
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-09-21 19:30:23 +0200

    add sendEmailService placeholder

---
+++ src/Services/SendEmailService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class SendEmailService
+{
+    public function send(string $to, string $subject, string $content, ?string $from = null): void 
+    {
+        
+    }
+}
