69ef79df690a9d7875dccf86caf56c108849463b
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-10-22 19:51:43 +0200

    Refacto constraint for ReviewType

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Range;
 
 class ReviewType extends AbstractType
 {
@@ -16,16 +17,25 @@ class ReviewType extends AbstractType
         $builder
             ->add('comment', TextareaType::class, [
                 'label' => 'Avis :',
-                'attr' => ['placeholder' => 'Exprimez votre ressenti sur ce trajet']
+                'attr' => [
+                    'placeholder' => 'Exprimez votre ressenti sur ce trajet',
+                ]
             ])
             ->add('grade', IntegerType::class, options: [
                 'attr' => [
                     'min' => 0,
                     'max' => 5
-
                 ],
                 'label' => 'Votre note : /5',
                 'mapped' => false,
+                'constraints' => [
+                    // Grade must be between 0 and 5
+                    new Range([
+                        'min' => 0,
+                        'max' => 5,
+                        'notInRangeMessage' => 'La note doit être comprise entre {{ min }} et {{ max }}.',
+                    ]),
+                ],
 
             ])
         ;
