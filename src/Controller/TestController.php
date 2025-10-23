51234798247bc47825b11a8807c86938d24f588c
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-10-23 19:12:06 +0200

    Refactor the UserPreferenceControlelr

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -8,10 +8,12 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(MongoFilterService $test)
     {
         $test->getUserPreferences(3);
--- src/Controller/UserPreferencesController.php
+++ src/Controller/UserPreferencesController.php
@@ -8,6 +8,7 @@ use App\Document\UserPreferences;
 use App\Repository\UserPreferencesRepository;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,23 +16,28 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class UserPreferencesController extends AbstractController
 {
+    private User $user;
+    public function __construct(private Security $security)
+    {
+        $this->user = $this->security->getUser();
+    }
+
     #[Route('/user/preferences', name: 'app_user_preferences')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     #[IsGranted('ROLE_DRIVER')]
     public function preferenceSettings(Request $request, DocumentManager $documentManager): Response
     {
-        /** @var User */
-        $user = $this->getUser();
+
 
         /** @var UserPreferencesRepository */
         $userPreferencesRepo = $documentManager->getRepository(UserPreferences::class);
 
         /** @var UserPreferences */
-        $preferences = $userPreferencesRepo->findByUserId($user->getId());
+        $preferences = $userPreferencesRepo->findByUserId($this->user->getId());
 
         if (!$preferences) {
             $preferences = new UserPreferences();
-            $preferences->setUserId($user->getId());
+            $preferences->setUserId($this->user->getId());
         }
 
         $form = $this->createForm(UserPreferencesType::class, $preferences);
