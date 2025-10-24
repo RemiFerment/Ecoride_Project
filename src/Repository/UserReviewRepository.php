5cb062d0f393bb59acf12f06e5f8deaf91dcfd24
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-10-24 16:25:09 +0200

    Add check reviews for moderator.

--- src/Repository/UserReviewRepository.php
+++ src/Repository/UserReviewRepository.php
@@ -16,28 +16,54 @@ class UserReviewRepository extends ServiceEntityRepository
         parent::__construct($registry, UserReview::class);
     }
 
-//    /**
-//     * @return UserReview[] Returns an array of UserReview objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
 
-//    public function findOneBySomeField($value): ?UserReview
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findByToBeChecked(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->join('u.review', 'r')
+            ->addSelect('r')
+            ->andWhere('r.statut = :status')
+            ->setParameter('status', 'TO_BE_CHECKED')
+            ->setMaxResults(500)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @return UserReview[] Returns an array of UserReview objects with gradeGiven < 3
+     */
+    public function findByLowGrade(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.gradeGiven < :grade')
+            ->setParameter('grade', 3)
+            ->orderBy('u.id', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    //    /**
+    //     * @return UserReview[] Returns an array of UserReview objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('u.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?UserReview
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
